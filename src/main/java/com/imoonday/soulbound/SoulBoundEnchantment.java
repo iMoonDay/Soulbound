60918161ab5730fb620d2e2e540aa894bb07e60e
iMoonDay <562613736@qq.com>
2023-08-19 09:42:10 +0800

    0.3

---
+++ src/main/java/com/imoonday/soulbound/SoulBoundEnchantment.java
@@ -0,0 +1,73 @@
+package com.imoonday.soulbound;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.GameRules;
+import org.jetbrains.annotations.NotNull;
+
+public class SoulBoundEnchantment extends Enchantment {
+    protected SoulBoundEnchantment() {
+        super(Rarity.RARE, EnchantmentCategory.BREAKABLE, EquipmentSlot.values());
+    }
+
+    @Override
+    public int getMinCost(int level) {
+        return Config.minPower;
+    }
+
+    @Override
+    public int getMaxCost(int level) {
+        int powerRange = Config.powerRange;
+        if (powerRange < 0) {
+            powerRange = 50;
+        }
+        return this.getMinCost(level) + powerRange;
+    }
+
+    @Override
+    public boolean isTreasureOnly() {
+        return Config.isTreasure;
+    }
+
+    @Override
+    public boolean canEnchant(ItemStack stack) {
+        return stack.isDamageableItem() || !stack.isStackable() || super.canEnchant(stack);
+    }
+
+    @Override
+    protected boolean checkCompatibility(@NotNull Enchantment other) {
+        return (other != Enchantments.VANISHING_CURSE || !Config.conflictWithVanishingCurse) && super.checkCompatibility(other);
+    }
+
+    public static void copySoulBoundItems(ServerPlayer oldPlayer, ServerPlayer newPlayer, boolean alive) {
+        if (!alive && !(newPlayer.level().getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY) || oldPlayer.isSpectator())) {
+            for (int i = 0; i < oldPlayer.getInventory().getContainerSize(); i++) {
+                ItemStack oldStack = oldPlayer.getInventory().getItem(i);
+                ItemStack newStack = newPlayer.getInventory().getItem(i);
+                int level = EnchantmentHelper.getTagEnchantmentLevel(Soulbound.SOUL_BOUND_ENCHANTMENT.get(), oldStack);
+                if (level > 0 && !ItemStack.matches(oldStack, newStack)) {
+                    if (Config.maxDamagePercent != 0 && !oldPlayer.isCreative()) {
+                        oldStack.hurt(oldPlayer.getRandom().nextInt(oldStack.getMaxDamage() * Config.maxDamagePercent / 100), oldPlayer.getRandom(), oldPlayer);
+                        if (oldStack.getDamageValue() >= oldStack.getMaxDamage()) {
+                            if (Config.allowBreakItem) {
+                                continue;
+                            } else {
+                                oldStack.setDamageValue(oldStack.getMaxDamage() - 1);
+                            }
+                        }
+                    }
+                    if (newStack.isEmpty()) {
+                        newPlayer.getInventory().setItem(i, oldStack);
+                    } else {
+                        newPlayer.getInventory().placeItemBackInInventory(oldStack);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/imoonday/soulbound/Soulbound.java
@@ -0,0 +1,27 @@
+package com.imoonday.soulbound;
+
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod(Soulbound.MODID)
+public class Soulbound {
+
+    public static final String MODID = "soulbound";
+    public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, MODID);
+    public static final RegistryObject<Enchantment> SOUL_BOUND_ENCHANTMENT = ENCHANTMENTS.register(MODID, SoulBoundEnchantment::new);
+
+    public Soulbound() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ENCHANTMENTS.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+}
